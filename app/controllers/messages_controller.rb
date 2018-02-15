faa534d0d15cc2067242558bc774d1b32e9f807b
Sky Xu <skyrocks88xmz@gmail.com>
2018-02-15 12:32:36 -0800

    fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-
+  skip_before_action :authenticate_user!, only: [:create], raise: false
   # this makes @chosen_recipient either contains a user record or a nil value.
   def new
     @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
